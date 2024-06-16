0be1b6c2bbb9961a82ce351d4f3f86b5dcfea6d6
xuexiji <xuexiji@123.cn>
2024-06-16 18:29:11 +0800

    first commit

---
+++ src/layouts/BasicLayout.vue
@@ -0,0 +1,11 @@
+<template>
+  <a-layout style="height: 400px">
+    <a-layout-header>导航栏</a-layout-header>
+    <a-layout-content>
+      <router-view />
+    </a-layout-content>
+    <a-layout-footer>学习基</a-layout-footer>
+  </a-layout>
+</template>
+<style></style>
+<script setup lang="ts"></script>
