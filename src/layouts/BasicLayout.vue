70953ce39ccc55384fd48b96ad55e789eb1c50aa
timepasses <107278149+timepasses@users.noreply.github.com>
2024-06-17 20:13:55 +0800

    Update README.md

--- src/layouts/BasicLayout.vue
+++ src/layouts/BasicLayout.vue
@@ -1,11 +1,41 @@
 <template>
-  <a-layout style="height: 400px">
-    <a-layout-header>导航栏</a-layout-header>
-    <a-layout-content>
-      <router-view />
-    </a-layout-content>
-    <a-layout-footer>学习基</a-layout-footer>
-  </a-layout>
+  <div id="basiclayout">
+    <a-layout style="height: 400px">
+      <a-layout-header class="header">
+        <GlobalHeader />
+      </a-layout-header>
+      <a-layout-content class="content">
+        <router-view />
+      </a-layout-content>
+      <a-layout-footer class="footer">学习基</a-layout-footer>
+    </a-layout>
+  </div>
 </template>
-<style></style>
-<script setup lang="ts"></script>
+<style>
+#basiclayout {
+}
+
+#basiclayout .header {
+  background: aqua;
+  color: #42b983;
+  margin-bottom: 16px;
+}
+
+#basiclayout .content {
+  background: linear-gradient(to right, #eee, #0000ff);
+  margin-bottom: 16px;
+}
+
+#basiclayout .footer {
+  background: antiquewhite;
+  padding: 16px;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  text-align: center;
+}
+</style>
+<script setup lang="ts">
+import GlobalHeader from "@/components/GlobalHeader.vue";
+</script>
