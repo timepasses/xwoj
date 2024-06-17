29f5531e30634bcd999d8084cd68013231a3fb71
xuexiji <xuexiji@123.cn>
2024-06-17 20:15:23 +0800

    Update README.md

---
+++ src/components/GlobalHeader.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="globalHeader">
+    <a-menu
+      mode="horizontal"
+      :default-selected-keys="['1']"
+      @menu-item-click="doMenuClick"
+    >
+      <a-menu-item
+        key="0"
+        :style="{ padding: 0, marginRight: '38px' }"
+        disabled
+      >
+        <div class="title-bar">
+          <img class="logo" src="../assets/oj-logo.jpg" />
+          <div class="title">XXJ-OJ</div>
+        </div>
+      </a-menu-item>
+      <a-menu-item v-for="item in routes" :key="item.path">
+        {{ item.name }}
+      </a-menu-item>
+    </a-menu>
+  </div>
+</template>
+<script setup lang="ts">
+import { routes } from "@/router/routes";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+const doMenuClick = (key: string) => {
+  router.push({
+    path: key,
+  });
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.title-bar {
+  display: flex;
+  align-items: center;
+}
+
+.title {
+  color: #42b983;
+  margin-left: 18px;
+}
+
+.logo {
+  height: 48px;
+}
+</style>
